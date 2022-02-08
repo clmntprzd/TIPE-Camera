ad04acebc7c670b54e60469cc2ba4395d5b8b904
Clément <clementperezduarte@gmail.com>
2022-02-08 20:05:39 +0100

    Rien

--- poc.py
+++ poc.py
@@ -1,2 +1,9 @@
 from graph_tool.all import *
-g=Graph()
+g = Graph(directed=False)
+v1 = g.add_vertex()
+v2 = g.add_vertex()
+v3 = g.add_vertex()
+v4 = g.add_vertex()
+e = g.add_edge(v1, v2)
+e2 = g.add_edge(v2, v3)
+e3 = g.add_edge(v3, v4)
